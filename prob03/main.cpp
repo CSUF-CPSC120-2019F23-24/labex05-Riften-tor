8a2b782c972b03f722fa57ea3d6704e97c80b36d
Riften <riften@csu.fullerton.edu>
2019-09-28 15:56:15 -0700

    Work in progress

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // Random Rectangle
+// Name: Annie Van
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+  // This program prints out a randomly sized Rectangle
+
+  int height = 3;
+
+  unsigned seed = time(0);
+  srand(seed);
+
+  int length = rand() %
+}
