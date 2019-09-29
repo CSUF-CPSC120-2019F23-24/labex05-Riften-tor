e6a5a65e8a87c32f08b7df875bf90ee8b4c20654
Riften-tor <riften@csu.fullerton.edu>
2019-09-28 19:54:34 -0700

    Fixed bugs

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -1,4 +1,4 @@
-git add// This program asks the user for their first name and middle/last initials
+// This program asks the user for their first name and middle/last initials
 // and outputs their user name.
 
 #include <iostream>
@@ -20,7 +20,7 @@ int main()
 
   // create username out of user's first name and middle/last initials
   std::string username2 = first_name + middle_initial + last_initial;
-  std::cout << "Your user name is \"" << username2 << " \"";
+  std::cout << "Your user name is \"" << username2 << "\"" << std::endl;
 
   return 0;
 }
