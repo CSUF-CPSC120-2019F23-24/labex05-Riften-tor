548691ec0bb9d01ed24ed6825956327ba180a816
Riften-tor <riften@csu.fullerton.edu>
2019-09-28 23:39:00 -0700

    Created program to make random rectangles

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,16 +1,23 @@
 // Random Rectangle
-// Name: Annie Van
 
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
 int main() {
-  // This program prints out a randomly sized Rectangle
-
-  int height = 3;
-
+  // Set randomizer seed
   unsigned seed = time(0);
   srand(seed);
 
-  int length = rand() %
+  // Maximum addend allowed for the length of the rectangle
+  const int MAX_ADDEND = 40;
+
+  std::string length;
+  int s;
+  s = rand() % (MAX_ADDEND + 1);
+  length.assign(s, '#');
+
+  std::cout << length << std::endl;
+  std::cout << length << std::endl;
+  std::cout << length << std::endl;
 }
